f286feedfa633e855cc2aab0cd295cf08ffb0ca4
Joao Lucas <joao.santos@nappsolutions.com>
2023-06-14 13:56:00 -0300

    feat: implementado conexões com a base de dados e criação de paginas staticas

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -18,7 +18,11 @@ import {
   orderBy,
   where,
   onSnapshot,
+  doc,
+  deleteDoc,
 } from "firebase/firestore";
+
+import Link from "next/link";
 
 interface HomeProps {
   user: {
@@ -26,9 +30,18 @@ interface HomeProps {
   };
 }
 
+interface TaskProps {
+  id: string;
+  created: Date;
+  public: boolean;
+  tarefa: string;
+  user: string;
+}
+
 export default function Dashboard({ user }: HomeProps) {
   const [input, setInput] = useState("");
   const [publicTask, setPublicTask] = useState(false);
+  const [tasks, setTasks] = useState<TaskProps[]>([]);
 
   useEffect(() => {
     async function loadTarefas() {
@@ -41,7 +54,18 @@ export default function Dashboard({ user }: HomeProps) {
       );
 
       onSnapshot(q, (snapshot) => {
-        console.log(snapshot);
+        let lista = [] as TaskProps[];
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            tarefa: doc.data().tarefa,
+            created: doc.data().created,
+            user: doc.data().user,
+            public: doc.data().public,
+          });
+        });
+        setTasks(lista);
       });
     }
 
@@ -70,6 +94,20 @@ export default function Dashboard({ user }: HomeProps) {
     } catch (err) {
       console.log(err);
     }
+  }
+
+  async function handleShare(id: string) {
+    await navigator.clipboard.writeText(
+      `${process.env.NEXT_PUBLIC_URL}/task/${id}`
+    );
+
+    alert("URL COPIADA");
+  }
+
+  async function handleDeleteTask(id: string) {
+    const docRef = doc(db, "tarefas", id);
+
+    await deleteDoc(docRef);
   }
 
   return (
@@ -106,20 +144,38 @@ export default function Dashboard({ user }: HomeProps) {
         </section>
         <section className={styles.taskContainer}>
           <h1>Minhas Tarefas</h1>
-          <article className={styles.task}>
-            <div className={styles.tagContainer}>
-              <label className={styles.tag}>Publica</label>
-              <button className={styles.shareButton}>
-                <FiShare2 size={22} color="#3183ff" />
-              </button>
-            </div>
-            <div className={styles.taskContent}>
-              <p>Minha primeira tarefa de exemplo, show demais!</p>
-              <button className={styles.trashButton}>
-                <FaTrash size={24} color="#ea3140" />
-              </button>
-            </div>
-          </article>
+          {tasks.map((item) => (
+            <article key={item.id} className={styles.task}>
+              {item.public && (
+                <div className={styles.tagContainer}>
+                  <label className={styles.tag}>Publica</label>
+                  <button
+                    className={styles.shareButton}
+                    onClick={() => handleShare(item.id)}
+                  >
+                    <FiShare2 size={22} color="#3183ff" />
+                  </button>
+                </div>
+              )}
+              <div className={styles.taskContent}>
+                {item.public ? (
+                  <Link href={`/task/${item.id}`}>
+                    <p>{item.tarefa}</p>
+                  </Link>
+                ) : (
+                  <p>{item.tarefa}</p>
+                )}
+                <button
+                  className={styles.trashButton}
+                  onClick={() => {
+                    handleDeleteTask(item.id);
+                  }}
+                >
+                  <FaTrash size={24} color="#ea3140" />
+                </button>
+              </div>
+            </article>
+          ))}
         </section>
       </main>
     </div>
